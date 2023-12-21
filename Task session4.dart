a44bbce56dbf370d8d2d439a4cc5846152a9e43a
karim1256 <153877200+karim1256@users.noreply.github.com>
2023-12-21 17:49:18 +0200

    Add files via upload

---
+++ Task session4.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi' ;
+Void? main() {
+  print(reverse('my name is kemo'));
+}
+
+
+String? reverse(String input) {
+  List<String> words = input.split(' ');
+  List<String> rWords = List.from(words.reversed);
+ String w=rWords.toString();
+  return w;
+}
+
